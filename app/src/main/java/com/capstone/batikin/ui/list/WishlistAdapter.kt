bdfa0d5a9b36f07f2aac57b61690db1070898b77
Aditya Fadhil Herindro <af.herindro@gmail.com>
2023-05-23 11:18:44 +0700

    detail activity

--- app/src/main/java/com/capstone/batikin/ui/list/WishlistAdapter.kt
+++ app/src/main/java/com/capstone/batikin/ui/list/WishlistAdapter.kt
@@ -1,14 +1,18 @@
 package com.capstone.batikin.ui.list
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.res.painterResource
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.capstone.batikin.R
 import com.capstone.batikin.databinding.WishlistItemBinding
 import com.capstone.batikin.model.Batik
+import com.capstone.batikin.ui.screen.detail.DetailActivity
 
 class WishlistAdapter(private val dataList: ArrayList<Batik>) : RecyclerView.Adapter<WishlistAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -30,6 +34,20 @@ class WishlistAdapter(private val dataList: ArrayList<Batik>) : RecyclerView.Ada
                     binding.ivHeartWishlist.setImageResource(R.drawable.baseline_favorite_24)
                 } else {
                     binding.ivHeartWishlist.setImageResource(R.drawable.baseline_favorite_border_24)
+                }
+                holder.itemView.setOnClickListener {
+                    val context = holder.itemView.context
+                    val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+                    // DataDummy
+                    // nanti dihapus
+                    intent.putExtra("id_extra", this.id)
+                    intent.putExtra("photo_extra", this.photoUrl)
+                    intent.putExtra("title_extra", this.title)
+                    intent.putExtra("price_extra", this.price)
+                    intent.putExtra("desc_extra", this.desc)
+                    intent.putExtra("favourite_extra", this.isFavourite)
+
+                    context.startActivity(intent)
                 }
             }
         }

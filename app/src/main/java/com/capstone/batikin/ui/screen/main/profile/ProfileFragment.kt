5e3903fd3f1100ff90d809873fa8661b15a1ec21
Lanalan77 <alan.prastowo@student.umn.ac.id>
2023-05-23 20:27:17 +0700

    buat profile button pake style jadi aneh pas diklik

---
+++ app/src/main/java/com/capstone/batikin/ui/screen/main/profile/ProfileFragment.kt
@@ -0,0 +1,43 @@
+package com.capstone.batikin.ui.screen.main.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.batikin.R
+import com.capstone.batikin.databinding.FragmentDashboardBinding
+import com.capstone.batikin.databinding.FragmentProfileBinding
+import com.capstone.batikin.ui.screen.main.dashboard.DashboardViewModel
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

9124a2ad0913bca40da5e8f7347f2258aa9682fe
Lanalan77 <alan.prastowo@student.umn.ac.id>
2023-06-05 20:02:16 +0700

    fix back pas galeri gk jadi milih gmbr

--- app/src/main/java/com/capstone/batikin/ui/screen/main/home/HomeScreen.kt
+++ app/src/main/java/com/capstone/batikin/ui/screen/main/home/HomeScreen.kt
@@ -133,7 +133,7 @@ fun HomeContent(data: ArrayList<Batik>, navController: NavHostController) {
 //                horizontalArrangement = Arrangement.spacedBy(0.dp),
         modifier = Modifier.padding(vertical = 8.dp)
     ) {
-        items(data.filter { it.rating >= 4.2}, key = { it.id }) { item ->
+        items(data.filter { it.rating >= 4.2}, ) { item ->
             Item(
                 item = item,
                 navController = navController,
@@ -152,7 +152,7 @@ fun HomeContent(data: ArrayList<Batik>, navController: NavHostController) {
             .padding(10.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp) // Atur jarak antara item
     ) {
-        items(data, key = { item -> item.id }) { item ->
+        items(data,) { item ->
             Item(item = item, navController = navController, modifier = Modifier.fillMaxWidth())
         }
     }
